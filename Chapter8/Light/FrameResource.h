608d862257904647bb04bf6fe689601cc5c7503a
kang <kangyh04@gmail.com>
2020-11-14 13:12:21 +0900

    light

---
+++ Chapter8/Light/FrameResource.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "../../Common/d3dUtil.h"
+#include "../../Common/MathHelper.h"
+#include "../../Common/UploadBuffer.h"
+
+using namespace DirectX;
+using namespace Microsoft;
+
+struct ObjectConstants
+{
+	XMFLOAT4X4 World = MathHelper::Identity4x4();
+	XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+};
+
+struct PassConstants
+{
+	XMFLOAT4X4 View = MathHelper::Identity4x4();
+	XMFLOAT4X4 InvView = MathHelper::Identity4x4();
+	XMFLOAT4X4 Proj = MathHelper::Identity4x4();
+	XMFLOAT4X4 InvProj = MathHelper::Identity4x4();
+	XMFLOAT4X4 ViewProj = MathHelper::Identity4x4();
+	XMFLOAT4X4 InvViewProj = MathHelper::Identity4x4();
+	XMFLOAT3 EyePosW = { 0.0f, 0.0f, 0.0f };
+	float xbPerObjectPad1 = 0.0f;
+	XMFLOAT2 RenderTargetSize = { 0.0f, 0.0f };
+	XMFLOAT2 InvRenderTargetSize = { 0.0f, 0.0f };
+	float NearZ = 0.0f;
+	float FarZ = 0.0f;
+	float TotalTime = 0.0f;
+	float DeltaTime = 0.0f;
+	XMFLOAT4 AmbientLight = { 0.0f, 0.0f, 0.0f, 1.0f };
+	Light Lights[MaxLights];
+};
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT3 Normal;
+};
+
+struct FrameResource
+{
+public:
+	FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount);
+	FrameResource(const FrameResource& rhs) = delete;
+	FrameResource& operator=(const FrameResource& rhs) = delete;
+	~FrameResource();
+
+	WRL::ComPtr<ID3D12CommandAllocator> ComdListAlloc;
+
+	std::unique_ptr<UploadBuffer<PassConstants>> PassCB = NULL;
+	std::unique_ptr<UploadBuffer<MaterialConstants>> MaterialCB = NULL;
+	std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = NULL;
+
+	UINT Fence = 0;
+};
