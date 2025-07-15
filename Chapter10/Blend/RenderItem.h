8212bdd4e33f57c1d550442acaf80b82bd3fd43b
kangyh04 <kangyh04@gmail.com>
2025-07-15 20:35:21 +0900

    blend app without shader

---
+++ Chapter10/Blend/RenderItem.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "MathHelper.h"
+#include "UploadBuffer.h"
+
+struct RenderItem
+{
+	RenderItem() = default;
+
+	XMFLOAT4X4 World = MathHelper::Identity4x4();
+	XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+
+	int NumFramesDirty = gNumFrameResources;
+
+	UINT ObjCBIndex = -1;
+
+	MeshGeometry* Geo = nullptr;
+	Material* Mat = nullptr;
+
+	D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	int baseVertexLocation = 0;
+};
+
+enum class RenderLayer : int
+{
+	Opaque = 0,
+	Transparent,
+	AlphaTested,
+	Count
+};
