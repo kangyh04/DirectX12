7ef21c67c87ff65f873e5fa0b5d2923ab94232a7
kangyh04 <kangyh04@gmail.com>
2025-08-23 10:41:04 +0900

    add default project and implement camera

---
+++ Chapter15/CameraAndDynamicIndexing/CameraAndDynamicIndexing.cpp
@@ -0,0 +1,14 @@
+#include "BaseApp.h"
+
+class CameraAndDynamicIndexingApp : public BaseApp
+{
+public:
+	CameraAndDynamicIndexingApp(HINSTANCE hInstance);
+	CameraAndDynamicIndexingApp(const CameraAndDynamicIndexingApp& rhs) = delete;
+	CameraAndDynamicIndexingApp& operator=(const CameraAndDynamicIndexingApp& rhs) = delete;
+	~CameraAndDynamicIndexingApp();
+
+	virtual bool Initialize() override;
+
+private:
+};
