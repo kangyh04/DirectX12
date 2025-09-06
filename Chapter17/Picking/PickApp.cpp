702bcff301f4fdb06537277361311f5ce421b303
kangyh04 <kangyh04@gmail.com>
2025-09-06 16:22:14 +0900

    picking

---
+++ Chapter17/Picking/PickApp.cpp
@@ -0,0 +1,260 @@
+#include "BaseApp.h"
+#include "Input.h"
+#include "TextureUtil.h"
+#include "StaticSamplers.h"
+#include "MeshUtil.h"
+#include "MaterialUtil.h"
+#include "PSOUtil.h"
+
+class PickApp : public BaseApp
+{
+public:
+	PickApp(HINSTANCE hInstance);
+	PickApp(const PickApp&) = delete;
+	PickApp& operator=(const PickApp&) = delete;
+	~PickApp();
+
+protected:
+	virtual void Build() override;
+
+private:
+	void LoadTextures();
+	void BuildRootSignature();
+	void BuildDescriptorHeaps();
+	void BuildShadersAndInputLayout();
+	void BuildCarGeometry();
+	void BuildFrameResources();
+	void BuildMaterials();
+	void BuildRenderItems();
+	void BuildPSOs();
+
+	void Pick(int sx, int sy);
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		PickApp theApp(hInstance);
+		if (!theApp.Initialize())
+		{
+			return 0;
+		}
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
+
+PickApp::PickApp(HINSTANCE hInstance)
+	:BaseApp(hInstance)
+{
+
+}
+
+PickApp::~PickApp()
+{
+	if (md3dDevice != nullptr)
+	{
+		FlushCommandQueue();
+	}
+}
+
+void PickApp::Build()
+{
+	LoadTextures();
+	BuildRootSignature();
+	BuildDescriptorHeaps();
+	BuildShadersAndInputLayout();
+	BuildCarGeometry();
+	BuildFrameResources();
+	BuildMaterials();
+	BuildRenderItems();
+	BuildPSOs();
+}
+
+void PickApp::LoadTextures()
+{
+	auto tex = TextureUtil::LoadTexture(md3dDevice.Get(), mCommandList.Get(), "white1x1");
+	mTextures[tex->Name] = move(tex);
+}
+
+void PickApp::BuildRootSignature()
+{
+	CD3DX12_DESCRIPTOR_RANGE texTable;
+	texTable.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 4, 0, 0);
+
+	CD3DX12_ROOT_PARAMETER slotRootParameter[4];
+
+	slotRootParameter[objRootParameterIndex].InitAsConstantBufferView(0);
+	slotRootParameter[passCBRootParameterIndex].InitAsConstantBufferView(1);
+	slotRootParameter[matBufferRootParameterIndex].InitAsShaderResourceView(0, 1);
+	slotRootParameter[texRootParameterIndex].InitAsDescriptorTable(1, &texTable, D3D12_SHADER_VISIBILITY_PIXEL);
+
+	auto staticSamplers = StaticSampler::GetStaticSamplers();
+
+	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(4, slotRootParameter,
+		(UINT)staticSamplers.size(), staticSamplers.data(),
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+	ComPtr<ID3DBlob> serializedRootSig = nullptr;
+	ComPtr<ID3DBlob> errorBlob = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1,
+		serializedRootSig.GetAddressOf(), errorBlob.GetAddressOf());
+
+	if (errorBlob != nullptr)
+	{
+		OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+	}
+	ThrowIfFailed(hr);
+
+	ThrowIfFailed(md3dDevice->CreateRootSignature(
+		0,
+		serializedRootSig->GetBufferPointer(),
+		serializedRootSig->GetBufferSize(),
+		IID_PPV_ARGS(mRootSignature.GetAddressOf())));
+}
+
+void PickApp::BuildDescriptorHeaps()
+{
+	D3D12_DESCRIPTOR_HEAP_DESC srvHeapDesc = {};
+	srvHeapDesc.NumDescriptors = 1;
+	srvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	srvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	ThrowIfFailed(md3dDevice->CreateDescriptorHeap(&srvHeapDesc, IID_PPV_ARGS(&mSrvDescriptorHeap)));
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE hDescriptor(mSrvDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
+
+	auto tex = mTextures["white1x1"]->Resource;
+
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.Format = tex->GetDesc().Format;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MostDetailedMip = 0;
+	srvDesc.Texture2D.MipLevels = tex->GetDesc().MipLevels;
+	srvDesc.Texture2D.ResourceMinLODClamp = 0.0f;
+	md3dDevice->CreateShaderResourceView(tex.Get(), &srvDesc, hDescriptor);
+}
+
+void PickApp::BuildShadersAndInputLayout()
+{
+	mShaders["standardVS"] = D3DUtil::CompileShader(L"Shaders\\Default.hlsl", nullptr, "VS", "vs_5_1");
+	mShaders["opaquePS"] = D3DUtil::CompileShader(L"shaders\\Default.hlsl", nullptr, "PS", "ps_5_1");
+
+	mStdInputLayout =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+	};
+}
+
+void PickApp::BuildCarGeometry()
+{
+	auto mesh = MeshUtil::LoadMesh(md3dDevice.Get(), mCommandList.Get(), "car");
+
+	mGeometries[mesh->Name] = move(mesh);
+}
+
+void PickApp::BuildFrameResources()
+{
+	for (int i = 0; i < gNumFrameResources; ++i)
+	{
+		mFrameResources.push_back(make_unique<FrameResource>(md3dDevice.Get(),
+			1, (UINT)mAllRitems.size(), (UINT)mMaterials.size()));
+	}
+}
+
+void PickApp::BuildMaterials()
+{
+	auto gray = MaterialUtil::LoadMaterial(0, 0, "gray0");
+	auto highlight = MaterialUtil::LoadMaterial(1, 0, "highlight0");
+
+	mMaterials[gray->Name] = move(gray);
+	mMaterials[highlight->Name] = move(highlight);
+}
+
+void PickApp::BuildRenderItems()
+{
+	auto carRitem = make_unique<RenderItem>();
+	XMStoreFloat4x4(&carRitem->World, XMMatrixScaling(1.0f, 1.0f, 1.0f) * XMMatrixTranslation(0.0f, 1.0f, 0.0f));
+	XMStoreFloat4x4(&carRitem->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	carRitem->ObjCBIndex = 0;
+	carRitem->Mat = mMaterials["gray0"].get();
+	carRitem->Geo = mGeometries["car"].get();
+	carRitem->PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	carRitem->Bounds = carRitem->Geo->DrawArgs["car"].Bounds;
+	carRitem->IndexCount = carRitem->Geo->DrawArgs["car"].IndexCount;
+	carRitem->StartIndexLocation = carRitem->Geo->DrawArgs["car"].StartIndexLocation;
+	carRitem->BaseVertexLocation = carRitem->Geo->DrawArgs["car"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(carRitem.get());
+
+	auto pickedRitem = make_unique<RenderItem>();
+	pickedRitem->World = MathHelper::Identity4x4();
+	pickedRitem->TexTransform = MathHelper::Identity4x4();
+	pickedRitem->ObjCBIndex = 1;
+	pickedRitem->Mat = mMaterials["highlight0"].get();
+	pickedRitem->Geo = mGeometries["car"].get();
+	pickedRitem->PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+	pickedRitem->Visible = false;
+
+	pickedRitem->IndexCount = 0;
+	pickedRitem->StartIndexLocation = 0;
+	pickedRitem->BaseVertexLocation = 0;
+	mRitemLayer[(int)RenderLayer::Highlight].push_back(pickedRitem.get());
+
+	mAllRitems.push_back(move(carRitem));
+	mAllRitems.push_back(move(pickedRitem));
+}
+
+void PickApp::BuildPSOs()
+{
+	auto opaquePSO = PSOUtil::MakeOpaquePSODesc(
+		mShaders["standardVS"].Get(),
+		mShaders["opaquePS"].Get(),
+		mStdInputLayout,
+		mRootSignature.Get(),
+		mBackBufferFormat,
+		mDepthStencilFormat,
+		m4xMsaaState,
+		m4xMsaaQuality);
+
+	mPsoDescs["opaque"] = opaquePSO;
+	ThrowIfFailed(md3dDevice->CreateGraphicsPipelineState(&opaquePSO, IID_PPV_ARGS(&mPSOs["opaque"])));
+
+	auto highlightPsoDesc = opaquePSO;
+
+	highlightPsoDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+
+	D3D12_RENDER_TARGET_BLEND_DESC transparencyBlendDesc;
+	transparencyBlendDesc.BlendEnable = true;
+	transparencyBlendDesc.LogicOpEnable = false;
+	transparencyBlendDesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	transparencyBlendDesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+	transparencyBlendDesc.BlendOp = D3D12_BLEND_OP_ADD;
+	transparencyBlendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+	transparencyBlendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+	transparencyBlendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+	transparencyBlendDesc.LogicOp = D3D12_LOGIC_OP_NOOP;
+	transparencyBlendDesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+
+	highlightPsoDesc.BlendState.RenderTarget[0] = transparencyBlendDesc;
+
+	mPsoDescs["highlight"] = highlightPsoDesc;
+	ThrowIfFailed(md3dDevice->CreateGraphicsPipelineState(&highlightPsoDesc, IID_PPV_ARGS(&mPSOs["highlight"])));
+}
+
+void PickApp::Pick(int sx, int sy)
+{
+
+}
---
+++ Chapter17/Picking/Shaders/Default.hlsl
@@ -0,0 +1,158 @@
+#ifndef NUM_DIR_LIGHTS
+#define NUM_DIR_LIGHTS 3
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+#define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+#define NUM_SPOT_LIGHTS 0
+#endif
+
+#include "LightingUtil.hlsl"
+
+struct ObjectData
+{
+    float4x4 World;
+    float4x4 TexTransform;
+    uint MaterialIndex;
+    uint Pad0;
+    uint Pad1;
+    uint Pad2;
+};
+
+struct MaterialData
+{
+    float4 DiffuseAlbedo;
+    float3 FresnelR0;
+    float Roughness;
+    float4x4 MatTransform;
+    uint DiffuseMapIndex;
+    uint MatPad0;
+    uint MatPad1;
+    uint MatPad2;
+};
+
+Texture2D gDiffuseMap[7] : register(t0);
+
+StructuredBuffer<ObjectData> gObjectData : register(t0, space1);
+StructuredBuffer<MaterialData> gMaterialData : register(t1, space1);
+
+SamplerState gsamPointWrap : register(s0);
+SamplerState gsamPointClamp : register(s1);
+SamplerState gsamLinearWrap : register(s2);
+SamplerState gsamLinearClamp : register(s3);
+SamplerState gsamAnisotropicWrap : register(s4);
+SamplerState gsamAnisotropicClamp : register(s5);
+
+cbuffer cbPass : register(b0)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+    float4 gAmbientLight;
+
+    // float4 gFogColor;
+    // float gFogStart;
+    // float gFogRange;
+    // float2 cbPerObjectPad2;
+    
+    Light gLights[MaxLights];
+};
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float3 NormalW : NORMAL;
+    float2 TexC : TEXCOORD;
+    
+    nointerpolation uint MatIndex : MATINDEX;
+};
+
+VertexOut VS(VertexIn vin, uint instanceID : SV_InstanceID)
+{
+    VertexOut vout = (VertexOut) 0.0f;
+
+    ObjectData objData = gObjectData[instanceID];
+    float4x4 world = objData.World;
+    float4x4 texTransform = objData.TexTransform;
+    uint matIndex = objData.MaterialIndex;
+
+    vout.MatIndex = matIndex;
+
+    MaterialData matData = gMaterialData[matIndex];
+    
+    float4 posW = mul(float4(vin.PosL, 1.0f), world);
+    vout.PosW = posW.xyz;
+
+    vout.NormalW = mul(vin.NormalL, (float3x3) world);
+    
+    vout.PosH = mul(posW, gViewProj);
+
+    float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), texTransform);
+    vout.TexC = mul(texC, matData.MatTransform).xy;
+
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    MaterialData matData = gMaterialData[pin.MatIndex];
+    float4 diffuseAlbedo = matData.DiffuseAlbedo;
+    float3 fresnelR0 = matData.FresnelR0;
+    float roughness = matData.Roughness;
+    uint diffuseTexIndex = matData.DiffuseMapIndex;
+
+    // float4 diffuseAlbedo = gDiffuseMap.Sample(gsamAnisotropicWrap, pin.TexC) * gDiffuseAlbedo;
+    diffuseAlbedo *= gDiffuseMap[diffuseTexIndex].Sample(gsamAnisotropicWrap, pin.TexC);
+
+// #ifdef ALPHA_TEST
+//     clip(diffuseAlbedo.a - 0.1f);
+// #endif
+
+    pin.NormalW = normalize(pin.NormalW);
+
+    float3 toEyeW = gEyePosW - pin.PosW;
+    // float distToEye = length(toEyeW);
+    // toEyeW /= distToEye;
+    
+    float4 ambient = gAmbientLight * diffuseAlbedo;
+
+    const float shininess = 1.0f - roughness;
+    Material mat = { diffuseAlbedo, fresnelR0, shininess };
+
+    float3 shadowFactor = 1.0f;
+    float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
+        pin.NormalW, toEyeW, shadowFactor);
+
+    float4 litColor = ambient + directLight;
+    
+// #ifdef FOG
+//     float fogAmount = saturate((distToEye - gFogStart) / gFogRange);
+//     litColor = lerp(litColor, gFogColor, fogAmount);
+// #endif
+    
+    litColor.a = diffuseAlbedo.a;
+    
+    return litColor;
+}
